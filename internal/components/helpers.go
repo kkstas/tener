41c007f2b2b039b25acbfed7ed9dc6a5f603c516
kkstas <stasiakkamilkrzysztof@gmail.com>
2024-12-08 22:31:29 +0100

    feat: filter by all available categories

--- internal/components/helpers.go
+++ internal/components/helpers.go
@@ -4,11 +4,32 @@ import (
 	"encoding/json"
 
 	"github.com/kkstas/tener/internal/model/expense"
+	"github.com/kkstas/tener/internal/model/expensecategory"
 )
 
 func toJSON(v any) string {
 	b, _ := json.Marshal(v)
 	return string(b)
+}
+
+func getUniqueCategoryNames(categoriesFromExpenses []string, categories []expensecategory.Category) []string {
+	m := make(map[string]bool)
+
+	for _, c := range categoriesFromExpenses {
+		m[c] = true
+	}
+
+	for _, c := range categories {
+		m[c.Name] = true
+	}
+
+	result := []string{}
+
+	for k := range m {
+		result = append(result, k)
+	}
+
+	return result
 }
 
 func extractCategories(expenses []expense.Expense) []string {
